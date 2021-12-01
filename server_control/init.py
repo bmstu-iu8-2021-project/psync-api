5ff9b60c90ef4b13d8827091516307e53e808d9c
petiayko <p.makretskii@gmail.com>
2021-12-01 17:57:35 +0300

    v1.13.12 some edit

--- server_control/init.py
+++ server_control/init.py
@@ -6,6 +6,6 @@ from db_control.init import create
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '80gp3beqsazmdl3wy1j617vho'
 # app.config['UPLOAD_FOLDER'] = '/home/peter/Study/03_semestr/Coursework/Storage/'  # local
-app.config['UPLOAD_FOLDER'] = '/data/storage/'  # volume
+app.config['UPLOAD_FOLDER'] = '/data/storage/'  # server
 sio = SocketIO(app)
 create()
